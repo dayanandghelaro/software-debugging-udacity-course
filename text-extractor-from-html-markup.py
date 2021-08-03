b0c0495dde8fbb54d1db639570fddb32a4c1b646
Dayanand Ghelaro <dayanandghelaro@gmail.com>
2021-08-03 11:15:47 +0500

    fix: fixed our first bug.

--- text-extractor-from-html-markup.py
+++ text-extractor-from-html-markup.py
@@ -1,15 +1,19 @@
 html_text = """
     <h1> Hello, World! </h1>
+    <a href= ">"> Hello, World! </a>
 """
 def extract_text(html_text):
     tag_mode = False
+    quote_mode = False
     text = ""
     for char in html_text:
-        if char == "<":
+        if not tag_mode and char == "<":
             tag_mode = True
-        elif char == ">":
+        elif char == "\"":
+            quote_mode = not quote_mode
+        elif not quote_mode and char == ">":
             tag_mode = False
-        elif not tag_mode:
+        elif not tag_mode and not quote_mode:
             text += char
     return text
 
